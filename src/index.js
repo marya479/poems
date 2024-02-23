fb4b555b8bae28ae583641b33ee3ecfd7b4afa28
marya479 <m.weglinska479@gmail.com>
2024-02-23 10:56:01 +0100

    ai addded

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "829fbdd0ctf5931e48b08422o27faf86";
+  
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
